7294e33a80f466d0a9f58bc0321e7e8b2c28b897
psidae <akson.rill@gmail.com>
2018-07-07 15:20:32 +0300

    @

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-  VALID_EMAIL_REGEX: /\A[\w+\-.]+0[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 60 },
                     uniqueness: { case_sensitive: false },
                     format: { width: VALID_EMAIL_REGEX }
