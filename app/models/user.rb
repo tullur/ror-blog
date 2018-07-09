096d161733ac10459c56d01acdaeb6750c4d94bf
psidae <akson.rill@gmail.com>
2018-07-09 12:15:13 +0300

    rm errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-  VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 60 },
                     uniqueness: { case_sensitive: false },
-                    format: { width: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_115221) do
+ActiveRecord::Schema.define(version: 2018_07_09_090107) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "update_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
