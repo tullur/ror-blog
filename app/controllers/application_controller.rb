eb2707c393888cfb394f59b18eff1bb9d6ec71ff
psidae <akson.rill@gmail.com>
2018-07-15 22:13:42 +0300

    color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
   def require_user
     if !logged_in?
       flash[:danger] = "You must be logged in to perform that action"
-      redirect_to root_path      
+      redirect_to root_path
+    end  
   end
 end
